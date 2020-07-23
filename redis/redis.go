4ee2c5c9ea55d54d2ee8267ffb0b799adb0cf1e0
wangliqiang <wangliqiang@easypay.com.cn>
2020-07-23 10:31:58 +0800

    增加redis订阅API

--- redis/redis.go
+++ redis/redis.go
@@ -1,8 +1,9 @@
 package redis
 
 import (
-	 "log"
-	 "os"
+	"context"
+	"log"
+	"os"
 	"sync"
 	"time"
 
@@ -270,4 +271,82 @@ func (c *Client) Stats() {
 	}
 Stop:
 	ticker.Stop()
+}
+
+func (c *Client) SubChannels(ctx context.Context,
+	onStart func() error,
+	onMessage func(channel string, data []byte) error,
+	channels ...string) error {
+	// A ping is set to the server with this period to test for the health of
+	// the connection and server.
+	const healthCheckPeriod = 20 * time.Second
+
+	psc := redis.PubSubConn{Conn: c.GetRedisConn()}
+
+	if err := psc.Subscribe(redis.Args{}.AddFlat(channels)...); err != nil {
+		return err
+	}
+
+	done := make(chan error, 1)
+
+	// Start a goroutine to receive notifications from the server.
+	go func() {
+		defer psc.Close()
+		for {
+			switch n := psc.Receive().(type) {
+			case error:
+				done <- n
+				return
+			case redis.Message:
+				if err := onMessage(n.Channel, n.Data); err != nil {
+					done <- err
+					return
+				}
+			case redis.Subscription:
+				switch n.Count {
+				case len(channels):
+					// Notify application when all channels are subscribed.
+					if err := onStart(); err != nil {
+						done <- err
+						return
+					}
+				case 0:
+					// Return from the goroutine when all channels are unsubscribed.
+					done <- nil
+					return
+				}
+			case redis.Pong:
+				continue
+			}
+
+		}
+	}()
+
+	ticker := time.NewTicker(healthCheckPeriod)
+	defer ticker.Stop()
+
+	var err error
+loop:
+	for {
+		select {
+		case <-ticker.C:
+			// Send ping to test health of connection and server. If
+			// corresponding pong is not received, then receive on the
+			// connection will timeout and the receive goroutine will exit.
+			if err = psc.Ping(""); err != nil {
+				break loop
+			}
+		case <-ctx.Done():
+			break loop
+		case err = <-done:
+			// Return error from the receive goroutine.
+			return err
+		}
+	}
+
+	// Signal the receiving goroutine to exit by unsubscribing from all channels.
+	psc.Unsubscribe()
+
+	// Wait for goroutine to complete.
+	return <-done
 }
